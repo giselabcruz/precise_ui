5de0f2ce24106f94de9a7d6334bfa5d42f830a61
giselabcruz <giselaisabelbelmontecruz@gmail.com>
2025-05-06 20:44:04 +0100

    create login and other views

---
+++ src/pages/Login/Login.tsx
@@ -0,0 +1,69 @@
+import React, { useState } from 'react';
+import './Login.css';
+
+interface LoginProps {
+  onLogin: (role: string) => void;
+}
+
+function Login({ onLogin }: LoginProps) {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log('Username:', username);
+    console.log('Password:', password);
+    const role = 'store-manager'; // Example role
+    onLogin(role)
+    // Aquí puedes agregar la lógica de autenticación
+  };
+  
+
+  return (
+    <div className="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
+      <form onSubmit={handleLogin} className="login-form relative py-3 sm:max-w-xl sm:mx-auto">
+        <div className="absolute inset-0 bg-gradient-to-r from-cyan-400 to-sky-500 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
+        <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
+          <div className="max-w-md mx-auto">
+            <div>
+              <h1 className='text-2xl font-semibold' >Login</h1>
+            </div>
+            <div className="divide-y divide-gray-200">
+              <div className="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
+                <div className="form-group relative">
+                  <label htmlFor="username" className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Username</label>
+                  <input
+                    type="text"
+                    id="username"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    placeholder="Enter your username"
+                    required
+                    className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600"
+                  />
+                </div>
+                <div className="form-group relative">
+                  <label htmlFor="password" className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Password</label>
+                  <input
+                    type="password"
+                    id="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    placeholder="Enter your password"
+                    required
+                    className='peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600'
+                  />
+                </div>
+                <div className="relative">
+                  <button type="submit" className="bg-cyan-500 text-white rounded-md px-2 py-1">Login</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
---
+++ src/pages/Supplier/Supplier.tsx
@@ -0,0 +1,18 @@
+import './Supplier.css';
+
+
+function Login() {
+  const handleLogin = () => {
+    const role = 'store-manager'; // Example role
+  };
+  
+
+  return (
+    <div className="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
+      <h1 className='text-2xl font-semibold'>Supplier</h1>
+      <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20"></div>
+    </div>
+  );
+};
+
+export default Login;
